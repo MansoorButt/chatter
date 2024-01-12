c6dc2b561d8099688034c99c283d7d6805cc40c8
Mansoor <balajmen@gmail.com>
2024-01-13 01:59:52 +0500

    first release

---
+++ src/config/stripe.ts
@@ -0,0 +1,31 @@
+export const PLANS = [
+    {
+        name:"Free",
+        slug: "free",
+        quota:10,
+        pagesPerPdf:10,
+        price:{
+            amount:0,
+            priceIds:{
+                test:'',
+                production:'',
+            }
+        }
+
+    },
+    {
+        name:"Pro",
+        slug: "pro",
+        quota:25,
+        pagesPerPdf:25,
+        price:{
+            amount:5,
+            priceIds:{
+                test:'price_1OXoVjIoIOsZpBxenAVylfcg',
+                production:'',
+            }
+        }
+
+    }
+
+]
